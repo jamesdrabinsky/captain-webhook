3da313ea3858adffa5214f77839746f3e3a245c5
kuanchiliao1 <kuanchiliao@gmail.com>
2024-05-27 14:56:55 -0700

    Add basic request logging
    Co-authored-by: jamesdrabinsky <jamesdrabinsky@users.noreply.github.com>
    Co-authored-by: bspowell <bspowell@users.noreply.github.com>
    Co-authored-by: Garrett <gjcochran@users.noreply.github.com>

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,20 @@
-console.log('test');
+import express from 'express';
 
-const a = 'd'
-const b: String = 4
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+  console.log(`Method: ${req.method}`);
+  console.log(`URL: ${req.url}`);
+  console.log('Headers:', req.headers);
+  console.log('Query Params:', req.query);
+  console.log('Body:', req.body);
+});
 
-const arr = [1, 2, 3]
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`),
+);
 
+// visit localhost:3000
+// assuming you have done 1) npm init 2) npm install express
