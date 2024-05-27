8b22a4c7c620e99f731dd1b345b508c25c54a255
Garrett Cochran <garrettcochran@Garretts-MacBook-Pro.local>
2024-05-27 17:26:28 -0600

    added some pseudo code for get / route

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; 
+
 const app = express();
 const port = 3000;
+
+app.use((req: any, _, next) => {
+  console.log('Subdomain:', req.headers.host.split('.')[0]);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -18,3 +25,5 @@ app.listen(port, () =>
 
 // visit localhost:3000
 // assuming you have done 1) npm init 2) npm install express
+
+// https://7440-76-23-45-191.ngrok-free.app
