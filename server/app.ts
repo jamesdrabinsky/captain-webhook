95e66246affbc3abf05f85d6e677fd6af8d761ce
Garrett Cochran <garrettcochran@Garretts-MacBook-Pro.local>
2024-05-28 16:26:04 -0600

    feat: added middleware to handle all types of requests to bin paths
    co-authored-by: Tony

--- server/app.ts
+++ server/app.ts
@@ -9,26 +9,28 @@ app.use((req: any, _, next) => {
   next();
 });
 
-app.get('/', (_, res) => {
-  res.send('<h1>Hello World!</h1>');
-  // console.log(`Method: ${req.method}`);
-  // console.log(`URL: ${req.url}`);
-  // console.log('Headers:', req.headers);
-  // console.log('Query Params:', req.query);
-  // console.log('Body:', req.body);
-  // const pushThisIntoMongo = {
-  //   headers: req.headers,
-  //   body: req.body
-    // ...
-  // }
-  // await the mongoId
-  // push info to psql request table using the object
+// // Middleware to handle all request methods for the same path
+app.use('/r/:bin_id', (req, res) => {
+  console.log(`Method: ${req.method}`);
+  console.log(`URL: ${req.url}`);
+  console.log('Headers:', req.headers);
+  console.log('Query Params:', req.query);
+  console.log('Body:', req.body);
 
-  
-  // 
+  res.send(`Handled ${req.method} request`);
 });
 
-app.get('/r', async (_, res) => {
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+  console.log(`Method: ${req.method}`);
+  console.log(`URL: ${req.url}`);
+  console.log('Headers:', req.headers);
+  console.log('Query Params:', req.query);
+  console.log('Body:', req.body);
+});
+
+// app.get('/api/create_new_bin', async (_, res) => {
+app.get('/r', async (_, res) => { 
   // create new bin id
   // add check to ensure not a duplicate
   // store bin id in postgres
@@ -39,9 +41,37 @@ app.get('/r', async (_, res) => {
   res.redirect(`/r/${binId}`)
 })
 
+// API
+app.get('/api/:bin_id/requests/:request_id', () => {
+  return {}
+})
+
+// below route represents the URL that users will use to see UI for a specific bin
+app.get('public/bins/:bin_id', () => {
+  return
+})
+
+
+// Handle incoming requests to bin
 app.get('/r/:bin_id', (_, res) => {
+  // console.log(`Method: ${req.method}`);
+  // console.log(`URL: ${req.url}`);
+  // console.log('Headers:', req.headers);
+  // console.log('Query Params:', req.query);
+  // console.log('Body:', req.body);
+  // this route handles get requests to the bin url
+ // https://726ce8044780.ngrok.app/r/05cd6ca264814
+  // const test_obj = req
+  // console.log('test')
+  // console.log(test_obj)
   res.send('<h1>Worked!</h1>')
 })
+app.post('r/:bin_id', (req) => {
+  console.log('body', req.body)
+})
+//app.delete('r/:bin_id')
+
+
 
 
 app.listen(port, () =>
